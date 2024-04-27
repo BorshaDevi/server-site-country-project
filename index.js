64df8e13868ed57f4748328ab8e5e4a490578988
Borshadevi <borshadevi27@gmail.com>
2024-04-27 16:38:11 +0600

    post data mongodb

--- index.js
+++ index.js
@@ -24,7 +24,14 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-
+  
+    const countryCollection = client.db("countryDB").collection("country");
+    app.post('/addSpots',async(req,res) => {
+      const user=req.body;
+      console.log(user)
+      const result = await countryCollection.insertOne(user);
+      res.send(result)
+    })
     
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
